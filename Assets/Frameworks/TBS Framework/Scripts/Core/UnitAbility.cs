4a6f2617c68b5b6efb139ae0d329dd9331cc05f0
Joe Baranoski <jbrnsk@gmail.com>
2019-07-14 21:00:20 -0400

    Implement main menu scene loading

--- Assets/Frameworks/TBS Framework/Scripts/Core/UnitAbility.cs
+++ Assets/Frameworks/TBS Framework/Scripts/Core/UnitAbility.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /// The purpose of this class is to prevent units from changing appearance from "finished" to any other than "normal".
 /// This was the cleanest way to do it that I could come up with. I'm thinking how it affects customizability and may change it in the future.
 /// </summary>
-public  class UnitAbility : MonoBehaviour 
+public class UnitAbility : MonoBehaviour 
 {
 
     public int AttackFactor;
---
+++ Assets/Scripts/UI/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public void LoadFirstScene(int level) {
+        SceneManager.LoadScene(level);
+    }
+}
